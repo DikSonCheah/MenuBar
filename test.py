cbc656fec456341800375928c63d2e3a32fdc69a
DikSonCheah <41046409+DikSonCheah@users.noreply.github.com>
2022-03-19 19:59:23 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-import argparse
-import os
+import os 
 from github import Github
 
-
-
-def create_issue(token):
-    g = Github()
-    for repo in g.get_user().get_repos():
-        print(repo.name)
-
-
-def main():
-    print(os.environ.get("token"))
-    create_issue(os.environ.get("token"))
-
-
-if __name__ == "__main__":
-    main()
-
+g = Github(os.environ.get('token'))
