13476460290b405d698cefccd75ef9bb43864b0f
DikSonCheah <41046409+DikSonCheah@users.noreply.github.com>
2022-03-19 19:28:22 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,24 @@
-import subprocess
+import argparse
+from github import Github
 
-p = subprocess.Popen("git log", shell=True, stdout=subprocess.PIPE, universal_newlines=True)
-print(p.communicate()[0])
+
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("token", help="Github authentication token")
+    args = parser.parse_args()
+    return args
+
+
+def create_issue(token):
+    g = Github()
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+
+
+def main():
+    args = arguments()
+    create_issue(args.token)
+
+
+if __name__ == "__main__":
+    main()
