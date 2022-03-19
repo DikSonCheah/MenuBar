e81e3a5fb971d0337ab8e2992a3525e823e53e0d
DikSonCheah <41046409+DikSonCheah@users.noreply.github.com>
2022-03-19 21:01:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import os
 from github import Github
 
 g = Github(os.environ.get('token'))
-for repo in g.get_user().get_repos():
-    print(repo.name)
+repo = g.get_repo("DikSonCheah/Menubar")
+repo.create_issue(title="This is a new issue", body="This is the issue body")
