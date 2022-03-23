879ecdd0a66ce671c0d891a0cf8cd317ec1cb0c3
DikSonCheah <41046409+DikSonCheah@users.noreply.github.com>
2022-03-23 19:02:07 +0800

    Add files via upload

--- .github/test.py
+++ .github/test.py
@@ -1,58 +1,84 @@
 from github import Github
 from datetime import datetime
 import os
+import time
+import re
 
-team = ["DikSonCheah", "kokwei217"]
+active_team = {,
+               }
+
 stale_branch_issues = []
-branch_infos = {}
+infos = {}
 
 
-def main():
+g = Github(os.environ.get('token'))
+repo = g.get_repo(os.environ.get('repo'))
+branches = repo.get_branches()
+issues = repo.get_issues()
 
-    g = Github(os.environ.get('token'))
-    repo = g.get_repo(os.environ.get('repo'))
-    branches = repo.get_branches()
-    issues = repo.get_issues()
 
-    for issue in issues:
-        if 'Stale branch' in issue.title:
-            stale_branch_issues.append(issue)
+# obtain all issues with stale branch keyword
+for issue in issues:
+    if 'Stale branch' in issue.title:
+        stale_branch_issues.append(issue)
 
-    for branch in branches:
-        commit = repo.get_commit(branch.name)
-        last_commitDate = commit.commit.author.date
-        commit_author = commit.commit.author.name
+for branch in branches:
 
-        duration = datetime.now() - last_commitDate
-        days = duration.days
+    commit = repo.get_commit(branch.commit.sha)
+    last_commitDate = commit.commit.author.date
+    committer_email = commit.commit.author.email
+
+    duration = datetime.now() - last_commitDate
+    days = duration.days
+
+    if days >= 0:
+
+        if 'github' in committer_email:
+            committer_email = re.search(r"(?<=\+)\w*", committer_email).group()
+
+        elif committer_email in active_team:
+            committer_email = active_team[committer_email]
 
         if stale_branch_issues:
+
             for issue in stale_branch_issues:
-                if branch.name in issue.title:
-                    branch_infos[branch.name] = [commit_author, last_commitDate, days, issue.title, issue.number]
-                    break
-                else:
-                    branch_infos[branch.name] = [commit_author, last_commitDate, days, None, None]
+                if committer_email in issue.title:
+
+                    if committer_email in infos:
+
+                        infos[committer_email][0].append(branch.name)
+                        infos[committer_email][1].append(str(days))
+
+                    elif committer_email not in infos:
+                        infos[committer_email] = [[branch.name], [str(days)], issue.title, issue.number]
         else:
-            branch_infos[branch.name] = [commit_author, last_commitDate, days, None, None]
 
-    for branch_info in branch_infos:
-        if branch_infos[branch_info][3] is None:
+            if committer_email in infos:
 
-            if branch_infos[branch_info][2] >= 1:
+                infos[committer_email][0].append(branch.name)
+                infos[committer_email][1].append(str(days))
 
-                if branch_infos[branch_info][0] in team:
-                    repo.create_issue(title=f"Stale branch detected, {branch_info}", body=f"{branch_info} last commited by {branch_infos[branch_info][0]} at {branch_infos[branch_info][1]}"
-                                      )
-
-                elif branch_infos[branch_info][0] not in team:
-                    repo.create_issue(title=f"Stale branch detected, {branch_info}", body=f"{branch_info} last commited by {branch_infos[branch_info][0]} at {branch_infos[branch_info][1]}"
-                                      )
-
-        elif branch_infos[branch_info][3] is not None:
-            issue_id = repo.get_issue(number=branch_infos[branch_info][4])
-            issue_id.create_comment("This branch is still stale")
+            elif committer_email not in infos:
+                infos[committer_email] = [[branch.name], [str(days)], None, None]
 
 
-if __name__ == "__main__":
-    main()
+for info in infos:
+    if infos[info][2] is None:
+
+        if info in active_team.values():
+
+            repo.create_issue(title="Stale branch/branches detected last commited by {} assigned to {}".format(info, info),
+                              body="These branch/branches {} commited from {} days ago respectively".format(", ".join(infos[info][0]), ", ".join(infos[info][1])))
+            time.sleep(1.5)
+
+        elif info not in active_team.values():
+
+            repo.create_issue(title="Stale branch/branches detected last commited by {} assigned to2 {}".format(info, info),
+                              body="These branch/branches {} commited from {} days ago respectively".format(", ".join(infos[info][0]), ", ".join(infos[info][1])))
+            time.sleep(1.5)
+
+    elif infos[info][2] is not None:
+        issue_id = repo.get_issue(number=infos[info][3])
+        issue_id.create_comment("These branch/branches {} are still stale".format(", ".join(infos[info][0])))
+        time.sleep(1.5)
+
